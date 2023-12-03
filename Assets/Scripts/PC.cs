910ed6c2e3fc3d9e92d59577f2d429abd7e5d838
Gabriel Sousa <gabrielsarubo@hotmail.com>
2023-12-03 19:27:41 -0300

    fix: add movement and rotation to PC

--- Assets/Scripts/PC.cs
+++ Assets/Scripts/PC.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private Rigidbody rbody;
+	public float speed = 5;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public float rotationSpeed = 80;
+	private Quaternion PCOriginalRotation;
+	private float counterXRotation = 0;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rbody = GetComponent<Rigidbody>();
+		// get the PC rotation values in the beginning of the game
+		PCOriginalRotation = transform.localRotation;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Movement
+		float moveForward = Input.GetAxis("Vertical");
+		float moveSideways = Input.GetAxis("Horizontal");
+		rbody.velocity = transform.TransformDirection(
+			new Vector3(moveSideways * speed, rbody.velocity.y, moveForward * speed)
+		);
+
+		// Rotation with mouse
+		counterXRotation += Input.GetAxis("Mouse X") * Time.deltaTime * rotationSpeed;
+		// get angle of the axis for the number (counterXRotation) in the X axis
+		Quaternion yRotation = Quaternion.AngleAxis(counterXRotation, Vector3.up);
+		transform.localRotation = PCOriginalRotation * yRotation;
+	}
 }

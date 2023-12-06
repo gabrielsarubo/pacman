98395ea726f4e9a686a4fbf3de44eb26a9eeaa7c
Gabriel Sousa <gabrielsarubo@hotmail.com>
2023-12-06 15:44:08 -0300

    feat: create Maze and bake new NavMeshSurface

--- Assets/Scripts/NavigationNPC.cs
+++ Assets/Scripts/NavigationNPC.cs
@@ -12,7 +12,7 @@ public class NavigationNPC : MonoBehaviour
 	private int index = 0;
 	public float maxDistance = 2;
 	private bool isChasing = false;
-	public float targetMaxDistance = 10;
+	public float targetMaxDistance = 10;// if NPC is 10 units or less away from the PC, chase the PC
 
 	// Start is called before the first frame update
 	void Start()
@@ -34,13 +34,13 @@ public class NavigationNPC : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		// if the PC get close to the NPC (how close is defined in maxDistance)
-		// then the NPC stops its default route and start chasing the PC
 		if (isChasing || Vector3.Distance(transform.position, player.position) <= targetMaxDistance)
 		{
 			isChasing = true;
 			agent.SetDestination(player.position);
 		}
+		// this condition is for the NPC to follow the patrolling route
+		// TODO what is the maxDistance for?
 		else if (Vector3.Distance(transform.position, agent.destination) < maxDistance)
 		{
 			nearest();
